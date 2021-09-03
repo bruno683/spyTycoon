32f43ffde431c4b5296f31da7133b95740c1856b
Bruno Richard <bruno683@outlook.fr>
2021-09-03 15:25:54 +0200

    Make crud missions on CRUD branch

---
+++ src/Form/MissionsType.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Missions;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class MissionsType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('title')
+            ->add('codeName')
+            ->add('country')
+            ->add('description')
+            ->add('type')
+            ->add('status')
+            ->add('startDate')
+            ->add('endDate')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Missions::class,
+        ]);
+    }
+}
