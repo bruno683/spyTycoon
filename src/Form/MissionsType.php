fd012486c99527d06be739b3decee7516ca2a0cf
Bruno Richard <bruno683@outlook.fr>
2021-09-04 11:47:26 +0200

    Modified MissionsType.php

--- src/Form/MissionsType.php
+++ src/Form/MissionsType.php
@@ -2,8 +2,14 @@
 
 namespace App\Form;
 
+use App\Entity\Agents;
 use App\Entity\Missions;
+use App\Repository\AgentsRepository;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\ChoiceList\ChoiceList;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\CollectionType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -12,14 +18,21 @@ class MissionsType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('title')
+            ->add('title', null, ['label' => 'Titre'])
             ->add('codeName')
             ->add('country')
             ->add('description')
             ->add('type')
             ->add('status')
             ->add('startDate')
-            ->add('endDate');
+            ->add('endDate')
+            ->add('agents', EntityType::class, [
+                'class' => Agents::class, 'query_builder' => function (AgentsRepository $er) {
+                    return $er->createQueryBuilder('u')
+                        ->orderBy('u.lastName', 'DESC');
+                },
+                'choice_label' => 'lastName',
+            ]);
     }
 
     public function configureOptions(OptionsResolver $resolver)
